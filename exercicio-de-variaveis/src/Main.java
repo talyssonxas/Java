de712d1fb74964ad687be05b819537bfff63f886
Talysson Albuquerque <talysson_xas@outlook.com>
2022-09-24 17:37:09 -0300

    Adiciona o Exercico de Variaveis Let'sCode

---
+++ exercicio-de-variaveis/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        String nome = "Talysson";
+        System.out.println("Olá," + nome + "!");
+        int a;
+        int b = 2;
+        a = 3;
+        int soma = a + b;
+        float divisao = (float)a/b;
+        int multiplicacao = a * b;
+        System.out.println("A soma de " + a + " + " + b + " = " + soma);
+        System.out.println("A divisão de " + a + "/" + b + " = " + divisao);
+        System.out.println("A multiplicação de " + a + " * " + b + " = " + multiplicacao);
+    }
+}
